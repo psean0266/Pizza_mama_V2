3d5839acc05f3de1e2bccea3a026fb96182503bf
Abdoulaye KOULIBALY <koulibaly.pangnon@gmail.com>
2023-12-11 06:27:41 +0100

    CRUD

--- pizza_mama/Models/Pizza.cs
+++ pizza_mama/Models/Pizza.cs
@@ -1,6 +1,16 @@
-﻿namespace pizza_mama.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_mama.Models
 {
     public class Pizza
     {
+        public int PizzaId { get; set; }
+        [Display(Name = "Nom")]
+        public string nom { get ; set; }
+        [Display(Name = "Prix ($)")]
+        public float prix { get; set; }
+        [Display (Name = "Végétarienne")]
+        public bool vegetarienne {  get; set; }
+        public string ingredients { get; set; } 
     }
 }
